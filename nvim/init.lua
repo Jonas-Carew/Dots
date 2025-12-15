63707dc2de9423882c607d7433383440b4296c91
JonasC <carewj@oregonstate.edu>
2025-12-15 03:52:58 -0600

    updated nvim config - no lazy, no mason, yes fully working lsp-s and typst preview

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,106 @@
 
-require("config")
+local v = vim
+local o = v.opt
+local keymap = v.keymap.set
+
+v.pack.add({
+	-- Dependency for Neotree, Telescope
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	-- Dependency for Neotree
+	{ src = "https://github.com/MunifTanjim/nui.nvim" },
+	-- Dependency for Neotree
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	-- Colorscheme
+	{ src = "https://github.com/Mofiqul/adwaita.nvim" },
+	-- Bottom Line
+	{ src = "https://github.com/windwp/windline.nvim" },
+	-- LSP config
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	-- Typst Preview
+	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+	-- Git Gutter Bar
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+	-- File tree
+	{ src = "https://github.com/nvim-neo-tree/neo-tree.nvim" },
+	-- Telescope
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+})
+
+v.lsp.enable({
+	"clangd",	-- C
+	"lua_ls",	-- Lua
+	"pylsp",	-- Python
+	"tinymist",	-- Typst
+	"zls",		-- Zig
+})
+
+-- Options!
+o.number = true
+o.relativenumber = true
+o.mouse = 'a'
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = false
+o.smartindent = true
+o.clipboard = 'unnamedplus'
+o.undofile = true
+o.list = true
+o.colorcolumn = "81"
+o.signcolumn = "yes"
+o.listchars = {
+	tab = '  ',
+	multispace = '··',
+	extends = '⟩',
+	precedes = '⟨',
+	trail = '·',
+}
+
+-- Space as leader
+keymap("", "<Space>", "<Nop>")
+v.g.mapleader = " "
+v.g.maplocalleader = " "
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h")
+keymap("n", "<C-j>", "<C-w>j")
+keymap("n", "<C-k>", "<C-w>k")
+keymap("n", "<C-l>", "<C-w>l")
+
+-- Easuer recording playing
+keymap("n", "<leader>q", "@")
+
+-- Remove highlights
+keymap("n", "<leader>n", ":nohl<cr>")
+
+-- Open Neotree
+keymap("n", "<leader>e", ":Neotree toggle<cr>")
+
+-- Telescope commands
+keymap("n", "<leader>g", ":Telescope live_grep<cr>")
+keymap("n", "<leader>f", ":Telescope find_files<cr>")
+
+-- Quick normal (change to using capslock)
+keymap("i", "jk", "<ESC>")
+
+-- Hold onto shifts
+keymap("v", "<", "<gv")
+keymap("v", ">", ">gv")
+
+-- Ctrl + L in insert opens up brackets
+keymap("i", "<C-l>", "<ESC>O")
+
+-- Space + h opens lsp help
+--keymap("n", "<leader>h", ":lua vim.diagnostic.open_float()<cr>")
+keymap("n", "<leader>lf", v.lsp.buf.format)
+
+-- Special function to replace all double spaces with tabs
+v.cmd("command Tab2 %s/  /\t/g")
+
+-- Switch Colorscheme
+v.cmd("colorscheme adwaita")
+
+-- Setup Windline
+require("windline_conf")
+
+
 
