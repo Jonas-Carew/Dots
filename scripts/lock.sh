237599431238138824f15289346c7f28059dcc85
JonasC <carewj@oregonstate.edu>
2026-08-17 15:06:13 -0500

    Cleaned up lock script

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-# Creating pre-lock status
-
-#[[ $(nmcli radio wifi) = "enabled" ]] && STAT_WIFI="on" || STAT_WIFI="off"
-
-# Locking
-
 fish -c dark
 #loginctl suspend
 swaylock
-
-# Post unlock
-#nmcli radio wifi $STAT_WIFI
 
