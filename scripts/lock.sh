80c6608cffb608aec884215d084a33ea8ff1ca06
JonasC <carewj@oregonstate.edu>
2026-08-17 13:29:36 -0500

    Updated locking script to restart wifi to status at lock

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Creating pre-lock status
+
+[[ $(nmcli radio wifi) = "enabled" ]] && STAT_WIFI="on" || STAT_WIFI="off"
+
+# Locking
+
 fish -c dark
-loginctl suspend
+#loginctl suspend
 swaylock
+
+# Post unlock
+nmcli radio wifi $STAT_WIFI
+
